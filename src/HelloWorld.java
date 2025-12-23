92342fb9eebb11bc7ae84236e4f8f26c48ece6e0
NishaVijayamma <vsn4891@gmail.com>
2025-12-23 11:09:33 +0100

    Merry Christmas

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         //  new  christmas branch
         System.out.println("Merry Christmas!");
+        System.out.println("Glad påsk!");
     }
 }
